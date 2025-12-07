6ca7380460824f5a47a2050afbef531a5d4d093d
JohannWilliams <johann.williams01@gmail.com>
2025-12-06 23:41:59 -0600

    added data type to var and added lists

--- data_type/data_type.py
+++ data_type/data_type.py
@@ -1,8 +1,8 @@
 
-x = 5
-y = 5.5
-z = "5.55"
-a = [1, 2, 3]
+x: int = 5
+y: float = 5.5
+z: str = "5.55"
+a: list[int] = [1, 2, 3]
 
 print(x)
 print(type(x))
@@ -13,3 +13,11 @@ print(type(z))
 
 print(a)
 print(type(a))
+
+
+names: list[str] = ["bob", "Joe", "Sue", "James", "Charly", "Smith"]
+
+long_names: list[str] = [name for name in names if len(name) > 4]
+
+print(f"names: {names}")
+print(f"long names: {long_names}")
